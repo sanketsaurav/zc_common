490e20afb1dc7cfc8eff8d8d9639675d422e0d61
Brandon Birkholz <brandonjbirkholz@gmail.com>
2015-12-04 10:32:05 -0800

    Added setup files for pip install

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='zc_common',
+      version='0.0.1',
+      description="A collection of Python utils",
+      long_description='',
+      keywords='zerocater python util',
+      author='ZeroCater',
+      author_email='tech@zerocater.com',
+      url='https://github.com/ZeroCater/zc_common',
+      download_url='https://github.com/ZeroCater/zc_common/tarball/0.0.1',
+      license='MIT',
+      packages=['zc_common'],
+      classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP",
+      ]
+  )
