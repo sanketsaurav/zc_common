ab8b435a8152579f6309b971b8ed2234b5f86320
Hamed Ahmadi <hamed@zerocater.com>
2016-05-03 15:04:08 -0700

    Support for remote resource update using PATCH

--- zc_common/remote_resource/serializers.py
+++ zc_common/remote_resource/serializers.py
@@ -1,6 +1,11 @@
-from rest_framework.serializers import Serializer
+from rest_framework_json_api.serializers import ResourceIdentifierObjectSerializer
+
+from zc_common.remote_resource.models import RemoteResource
 
 
-class RemoteResourceSerializer(Serializer):
+class RemoteResourceSerializer(ResourceIdentifierObjectSerializer):
     def to_representation(self, instance):
         return { 'type': instance.type, 'id': instance.id}
+
+    def to_internal_value(self, data):
+        return RemoteResource(data['type'], data['id'])
--- zc_common/remote_resource/views.py
+++ zc_common/remote_resource/views.py
@@ -3,7 +3,9 @@ from rest_framework_json_api.views import RelationshipView
 from zc_common.remote_resource.models import RemoteResource
 from zc_common.remote_resource.serializers import RemoteResourceSerializer
 
+
 class RelationshipView(RelationshipView):
+    serializer_class = RemoteResourceSerializer
 
     def _instantiate_serializer(self, instance):
         if isinstance(instance, RemoteResource):
