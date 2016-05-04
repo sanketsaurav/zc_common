20d856cdd020aa6fe11465287e40d9caa141ebda
Hamed Ahmadi <hamed@zerocater.com>
2016-05-04 14:40:22 -0700

    Clean up to use zc_common ResourceIdentifierObjectSerializer

--- zc_common/remote_resource/views.py
+++ zc_common/remote_resource/views.py
@@ -1,15 +1,18 @@
+from django.db.models import Model
+from django.db.models.query import QuerySet
+from django.db.models.manager import Manager
 from rest_framework_json_api.views import RelationshipView
 
 from zc_common.remote_resource.models import RemoteResource
-from zc_common.remote_resource.serializers import RemoteResourceSerializer
+from zc_common.remote_resource.serializers import ResourceIdentifierObjectSerializer
 
 
 class RelationshipView(RelationshipView):
-    serializer_class = RemoteResourceSerializer
+    serializer_class = ResourceIdentifierObjectSerializer
 
     def _instantiate_serializer(self, instance):
         if isinstance(instance, RemoteResource):
-            return RemoteResourceSerializer(instance=instance)
+            return ResourceIdentifierObjectSerializer(instance=instance)
 
         if isinstance(instance, Model) or instance is None:
             return self.get_serializer(instance=instance)
