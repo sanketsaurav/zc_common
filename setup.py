cec422613e5d9a9661ca0c0d0641e26cf14e2a92
Hamed Ahmadi <hamed@zerocater.com>
2016-05-10 10:52:52 -0700

    Updating package version to 0.1.1

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ def get_packages(package):
             if os.path.exists(os.path.join(dirpath, '__init__.py'))]
 
 setup(name='zc_common',
-      version='0.1.0',
+      version='0.1.1',
       description="A collection of Python utils",
       long_description='',
       keywords='zerocater python util',
       author='ZeroCater',
       author_email='tech@zerocater.com',
       url='https://github.com/ZeroCater/zc_common',
-      download_url='https://github.com/ZeroCater/zc_common/tarball/0.1.0',
+      download_url='https://github.com/ZeroCater/zc_common/tarball/0.1.1',
       license='MIT',
       packages=get_packages('zc_common'),
       classifiers=[
