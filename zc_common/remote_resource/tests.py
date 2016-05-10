d70912f159c560745d5d0c8a8249f544e68011fa
Hamed Ahmadi <hamed@zerocater.com>
2016-05-09 17:39:31 -0700

    Helper method to validate existence of instance in a response

--- zc_common/remote_resource/tests.py
+++ zc_common/remote_resource/tests.py
@@ -12,11 +12,54 @@ class ResponseTestCase(APITestCase):
     FAILURE_HIGH_LEVEL_KEYS = ['errors']
     FAILURE_DATA_KEYS = ['status', 'source', 'detail']
 
+    INSTANCE_TOP_LEVEL_KEYS = ["type", "id", "attributes"]
+
     def convert_to_list(self, data):
-        if isinstance(data, list):
+        if isinstance(data, dict):
+            return [data]
+        elif isinstance(data, list):
             return data
         else:
-            return [data]
+            return data.all()
+
+    def validate_instance_in_data(self, data, instance, attributes, relationship_keys=None):
+        self.assertTrue(all(key in data for key in self.INSTANCE_TOP_LEVEL_KEYS))
+
+        instance_attributes = data["attributes"]
+        for key in attributes:
+            self.assertEqual(instance_attributes[camelize(key, False)], getattr(instance, key))
+
+        if not relationship_keys:
+            return True
+
+        relationships = data["relationships"]
+
+        for relationship_name in relationship_keys:
+            name = camelize(relationship_name, False)
+            self.assertTrue(name in relationships)
+            data = self.convert_to_list(relationships[name]["data"])
+            instance_relationship = self.convert_to_list(getattr(instance, relationship_name))
+
+            for data_object, instance_object in zip(data, instance_relationship):
+                self.assertEqual(data_object["id"], str(instance_object.id))
+
+                try:
+                    instance_type = instance_object.type
+                except:
+                    instance_type = instance_object.__class__.__name__
+
+                self.assertEqual(data_object["type"], instance_type)
+
+
+    def validate_instance_in_response(self, response, instance, attributes, relationship_keys=None):
+        data = self.load_json(response)["data"]
+        self.validate_instance_in_data(data, instance, attributes, relationship_keys)
+
+    def validate_instance_list_in_response(self, response, instances, attributes, relationship_keys=None):
+        instance_data = self.load_json(response)["data"]
+        for data, instance in zip(instance_data, instances):
+            self.validate_instance_in_data(data, instance, attributes, relationship_keys)
+
 
     def success_response_structure_test(self, response, status, relationship_keys=None):
         """
