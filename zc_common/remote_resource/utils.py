35a07ec7c408a78684dae9f4c6ca4622eb901113
Hamed Ahmadi <hamed@zerocater.com>
2016-06-30 15:04:00 -0700

    Updating based on implemented request wrappers

--- zc_common/remote_resource/utils.py
+++ zc_common/remote_resource/utils.py
@@ -1,8 +1,10 @@
 import requests
 
-def emit_event(event_type, event_name, resource_type, resource_id, user_id, meta_data=None):
-    create_event_url = get_route_from_fk(event_type)  # TBD
-    headers = get_interservice_headers()  # TBD
+from .request import make_service_request, get_route_from_fk
+
+
+def emit_event(service_name, event_type, event_name, resource_type, resource_id, user_id, meta_data=None):
+    create_event_url = get_route_from_fk(event_type)
 
     event_data = generate_event_data(event_type,
                                      event_name,
@@ -11,7 +13,7 @@ def emit_event(event_type, event_name, resource_type, resource_id, user_id, meta
                                      user_id,
                                      meta_data=meta_data)
 
-    response = requests.post(create_event_url, data=event_data, headers=headers)
+    response = make_service_request(service_name, create_event_url, 'post', event_data)
     return response
 
 
@@ -33,4 +35,4 @@ def generate_event_data(event_type, event_name, resource_type, resource_id, user
             'meta': meta_data
         })
 
-    return json.dumps(data)
+    return data
