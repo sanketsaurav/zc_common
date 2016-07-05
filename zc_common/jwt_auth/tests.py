b8bc5080d790d29887416f288dc17a9319f27d6a
Theophile Nsengimana <theo@zerocater.com>
2016-07-05 15:49:11 -0700

    Refactor get_anonymous_token to be called without a user_id.

--- zc_common/jwt_auth/tests.py
+++ zc_common/jwt_auth/tests.py
@@ -81,9 +81,8 @@ class AuthenticationMixin:
         user = self.create_user(['user'], user_id)
         return self.create_user_token(user)
 
-    def get_anonymous_token(self, user_id):
-        user = self.create_user(['anonymous'], user_id)
-        return self.create_user_token(user)
+    def get_anonymous_token(self):
+        return "JWT {}".format(jwt_encode_handler({'roles': ['anonymous']}))
 
     def get_service_token(self, service_name='Test'):
         return self.create_service_token(service_name)
