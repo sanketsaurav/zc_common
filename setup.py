a3d3f32824d0447a1b24145fd7be0683aa1c85e9
Brandon Birkholz <brandonjbirkholz@gmail.com>
2016-10-05 11:11:57 -0700

    Bump version to 0.2.4

--- setup.py
+++ setup.py
@@ -13,14 +13,14 @@ def get_packages(package):
 
 setup(
     name='zc_common',
-    version='0.2.3',
+    version='0.2.4',
     description="Shared code for ZeroCater microservices",
     long_description='',
     keywords='zerocater python util',
     author='ZeroCater',
     author_email='tech@zerocater.com',
     url='https://github.com/ZeroCater/zc_common',
-    download_url='https://github.com/ZeroCater/zc_common/tarball/0.2.3',
+    download_url='https://github.com/ZeroCater/zc_common/tarball/0.2.4',
     license='MIT',
     packages=get_packages('zc_common'),
     classifiers=[
