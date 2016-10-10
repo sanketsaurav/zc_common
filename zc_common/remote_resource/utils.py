02140469c47e8120e64d9bd41c0d59cac82cd1bf
Theophile Nsengimana <theo@zerocater.com>
2016-10-10 13:07:50 -0700

    Add emit_service_event function

--- zc_common/remote_resource/utils.py
+++ zc_common/remote_resource/utils.py
@@ -1,7 +1,16 @@
-from .request import make_service_request, get_route_from_fk
+import sys
+import warnings
+from .request import make_service_request, get_route_from_fk, ServiceRequestException
+
+
+class EmitEventFailure(Exception):
+    pass
 
 
 def emit_event(service_name, event_type, event_name, resource_type, resource_id, user_id, meta_data=None):
+    warnings.warn("Consider using emit_service_event to throw a proper exception when emission fails.",
+                  DeprecationWarning)
+
     create_event_url = get_route_from_fk(event_type)
 
     event_data = generate_event_data(
@@ -15,6 +24,16 @@ def emit_event(service_name, event_type, event_name, resource_type, resource_id,
 
     response = make_service_request(service_name, create_event_url, 'post', event_data)
     return response
+
+
+def emit_service_event(sender_service_name, event_type, event_name, resource_type, resource_id, user_id,
+                       meta_data=None):
+    try:
+        return emit_event(sender_service_name, event_type, event_name, resource_type, resource_id, user_id, meta_data)
+    except ServiceRequestException:
+        message = ("Error emitting {} to {}. resource_type: {}, resource_id: {}, user_id: {}, meta_data: {}"
+                   .format(event_name, event_type, resource_type, resource_id, user_id, meta_data))
+        raise EmitEventFailure(message), None, sys.exc_info()[2]
 
 
 def generate_event_data(event_type, event_name, resource_type, resource_id, user_id, meta_data=None):
