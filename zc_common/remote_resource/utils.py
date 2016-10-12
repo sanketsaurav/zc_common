15cfe8a3e6b58662a49b68861459b40335ea1bf9
Theophile Nsengimana <theo@zerocater.com>
2016-10-12 10:53:04 -0700

    Handle ServiceRequestExceptin in emit_event function

--- zc_common/remote_resource/utils.py
+++ zc_common/remote_resource/utils.py
@@ -8,28 +8,20 @@ class EmitEventFailure(Exception):
 
 
 def emit_event(service_name, event_type, event_name, resource_type, resource_id, user_id, meta_data=None):
-    warnings.warn("Consider using emit_service_event to throw a proper exception when emission fails.",
-                  DeprecationWarning)
-
-    create_event_url = get_route_from_fk(event_type)
-
-    event_data = generate_event_data(
-        event_type,
-        event_name,
-        resource_type,
-        resource_id,
-        user_id,
-        meta_data=meta_data
-    )
-
-    response = make_service_request(service_name, create_event_url, 'post', event_data)
-    return response
-
-
-def emit_service_event(sender_service_name, event_type, event_name, resource_type, resource_id, user_id,
-                       meta_data=None):
     try:
-        return emit_event(sender_service_name, event_type, event_name, resource_type, resource_id, user_id, meta_data)
+        create_event_url = get_route_from_fk(event_type)
+
+        event_data = generate_event_data(
+            event_type,
+            event_name,
+            resource_type,
+            resource_id,
+            user_id,
+            meta_data=meta_data
+        )
+
+        response = make_service_request(service_name, create_event_url, 'post', event_data)
+        return response
     except ServiceRequestException:
         message = ("Error emitting {} to {}. resource_type: {}, resource_id: {}, user_id: {}, meta_data: {}"
                    .format(event_name, event_type, resource_type, resource_id, user_id, meta_data))
