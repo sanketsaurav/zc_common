3431b831360e04d855c7e7eb709c578b1eb61f8c
amberylx <amberyl.x@gmail.com>
2016-10-25 12:52:58 -0700

    Rename S3 bucket, improve error handling, include event data

--- zc_common/email.py
+++ zc_common/email.py
@@ -9,7 +9,7 @@ from raven.contrib.django.raven_compat.models import client
 from zc_common.events.emit import emit_microservice_event
 
 
-S3_BUCKET_NAME = 'zc-email-template-test'
+S3_BUCKET_NAME = 'mp-email'
 EMAIL_EVENT_TYPE = 'send_email'
 ATTACHMENT_PREFIX = 'attachment_'
 
@@ -29,65 +29,63 @@ def get_s3_email_bucket():
     aws_access_key_id = settings.AWS_ACCESS_KEY_ID
     aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY
     if not (aws_access_key_id and aws_secret_access_key):
-        raise MissingCredentialsError
+        msg = 'You need to set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY in your settings file.'
+        raise MissingCredentialsError(msg)
 
     conn = boto.connect_s3(aws_access_key_id, aws_secret_access_key)
     bucket = conn.get_bucket(S3_BUCKET_NAME)
-    k = Key(bucket)
-    return k
+    return bucket
 
 
-def generate_s3_folder_name():
+def generate_s3_folder_name(email_uuid):
     email_date = date.today().isoformat()
     email_timestamp = int(time.time())
-    email_uuid = uuid.uuid4()
     return "{}/{}_{}".format(email_date, email_timestamp, email_uuid)
 
 
 def send_email(from_email=None, to=None, cc=None, bcc=None, reply_to=None,
                subject=None, plaintext_body=None, html_body=None, headers=None,
-               files=None, attachments=None):
+               files=None, attachments=None, user_id=None, resource_type=None, resource_id=None):
     """
     files:       A list of file paths
     attachments: A list of tuples of the format (filename, content_type, content)
     """
-    try:
-        bucket = get_s3_email_bucket()
-        s3_folder_name = generate_s3_folder_name()
-    except MissingCredentialsError:
-        raise
-    except:
-        send_error_to_sentry()
-        raise
+    email_uuid = uuid.uuid4()
+    bucket = get_s3_email_bucket()
+    s3_folder_name = generate_s3_folder_name(email_uuid)
 
     html_body_key = None
     if html_body:
         html_body_key = "{}/html".format(s3_folder_name)
-        bucket.key = html_body_key
-        bucket.set_contents_from_string(html_body)
+        k = Key(bucket)
+        k.key = html_body_key
+        k.set_contents_from_string(html_body)
 
     plaintext_body_key = None
     if plaintext_body:
         plaintext_body_key = "{}/plaintext".format(s3_folder_name)
-        bucket.key = plaintext_body_key
-        bucket.set_contents_from_string(plaintext_body)
+        k = Key(bucket)
+        k.key = plaintext_body_key
+        k.set_contents_from_string(plaintext_body)
 
     attachments_keys = []
     if attachments:
         for filename, mimetype, attachment in attachments:
             attachment_key = "{}/attachment_{}".format(s3_folder_name, filename)
-            bucket.key = attachment_key
-            bucket.set_contents_from_filename(attachment)
+            k = Key(bucket)
+            k.key = attachment_key
+            k.set_contents_from_filename(attachment)
             attachments_keys.append(attachment_key)
     if files:
         for filepath in files:
             filename = filepath.split('/')[-1]
             attachment_key = "{}/attachment_{}".format(s3_folder_name, filename)
-            bucket.key = attachment_key
-            bucket.set_contents_from_filename(filepath)
+            k = Key(bucket)
+            k.key = attachment_key
+            k.set_contents_from_filename(filepath)
             attachments_keys.append(attachment_key)
 
-    event_data = {
+    email_data = {
         'from_email': from_email,
         'to': to,
         'cc': cc,
@@ -99,4 +97,12 @@ def send_email(from_email=None, to=None, cc=None, bcc=None, reply_to=None,
         'attachments_keys': attachments_keys,
         'headers': headers,
     }
+
+    event_data = {
+        'user_id': user_id,
+        'resource_type': resource_type,
+        'resource_id': resource_id,
+    }
+    event_data.update(email_data)
+
     emit_microservice_event(EMAIL_EVENT_TYPE, **event_data)
