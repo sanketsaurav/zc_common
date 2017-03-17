2af6ec0de359d0678963ec4cf86a89f4f91e5978
Hamed Ahmadi <hamed@zerocater.com>
2017-03-16 17:24:23 -0700

    Pull phone_regex out

--- zc_common/fields.py
+++ zc_common/fields.py
@@ -7,14 +7,14 @@ from django.core.validators import RegexValidator
 class PhoneNumberField(models.CharField):
     description = "A field that checks the value be a valid phone number"
 
-    def __init__(self, *args, **kwargs):
-        phone_regex = RegexValidator(
-            regex=r'^(\+\d{1}\s?)?\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{4}$', message="Enter a valid Phone Number.")
+    phone_regex = RegexValidator(
+        regex=r'^(\+\d{1}\s?)?\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{4}$', message="Enter a valid Phone Number.")
 
+    def __init__(self, *args, **kwargs):
         if 'max_length' not in kwargs:
             kwargs['max_length'] = 20
 
         if 'validators' not in kwargs:
-            kwargs['validators'] = [phone_regex]
+            kwargs['validators'] = [self.phone_regex]
 
         super(PhoneNumberField, self).__init__(*args, **kwargs)
