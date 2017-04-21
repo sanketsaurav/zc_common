389e85d38557a574c58c81d48cdd287f360499b8
Tony Pilara <tony.pilara@gmail.com>
2017-04-21 10:37:56 -0700

    Add PKField which is a 10 digit random int

--- zc_common/fields.py
+++ zc_common/fields.py
@@ -1,12 +1,17 @@
 from __future__ import unicode_literals
 
 import re
+import uuid
 
 from django.db import models
 from django.core.validators import RegexValidator
 
 
 US_PHONE_FORMAT = r'^(\+1\s?)?\(?(\d{3})\)?[\s-]?(\d{3})[\s-]?(\d{4})$'
+
+
+def numeric_uuid_generator():
+    return str(uuid.uuid4().int)[:10]
 
 
 class PhoneNumberField(models.CharField):
@@ -33,3 +38,18 @@ class PhoneNumberField(models.CharField):
             return value
 
         return "({}) {}-{}".format(r.group(2), r.group(3), r.group(4))
+
+
+class PKField(models.CharField):
+    description = "A primary key field that defaults to a 10 digit random int"
+
+    def __init__(self, *args, **kwargs):
+        kwargs['primary_key'] = True
+
+        if 'default' not in kwargs:
+            kwargs['default'] = numeric_uuid_generator
+
+        if 'max_length' not in kwargs:
+            kwargs['max_length'] = 50
+
+        super(PKField, self).__init__(*args, **kwargs)
