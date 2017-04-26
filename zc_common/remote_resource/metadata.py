ae116b5d7e43f9e1b9c6260ff669af8f6aa95002
Brandon Birkholz <brandonjbirkholz@gmail.com>
2017-04-26 11:14:10 -0700

    Improve RelationshipMetadata get_field_info

--- zc_common/remote_resource/metadata.py
+++ zc_common/remote_resource/metadata.py
@@ -1,6 +1,9 @@
 from django.db.models.fields import related
+from rest_framework.relations import ManyRelatedField
 from rest_framework.utils.field_mapping import ClassLookupDict
 from rest_framework_json_api.metadata import JSONAPIMetadata
+from rest_framework_json_api.utils import get_related_resource_type
+
 from zc_common.remote_resource.relations import RemoteResourceField
 from zc_common.remote_resource.models import GenericRemoteForeignKey, RemoteForeignKey
 
@@ -16,12 +19,17 @@ class RelationshipMetadata(JSONAPIMetadata):
 
     def get_field_info(self, field):
         field_info = super(RelationshipMetadata, self).get_field_info(field)
-        if isinstance(field, RemoteResourceField):
-            model_class = getattr(field.parent.Meta, 'model')
-            model_field = getattr(model_class, field.field_name)
-            if hasattr(model_field, 'type'):
-                field_info['relationship_resource'] = model_field.type
-            else:
-                # Generic FKs have no resource type
-                field_info['relationship_resource'] = None
+        if isinstance(field, RemoteResourceField) or isinstance(field, ManyRelatedField):
+            model_class = field.parent.Meta.model
+            model_field = getattr(model_class, field.source)
+
+            field_info['relationship_type'] = self.relation_type_lookup[model_field]
+            field_info['relationship_resource'] = get_related_resource_type(field)
+
+            if field_info['relationship_resource'] == 'RemoteResource':
+                if hasattr(model_field, 'type'):
+                    field_info['relationship_resource'] = model_field.type
+                else:
+                    # Generic FKs have no resource type
+                    field_info['relationship_resource'] = None
         return field_info
