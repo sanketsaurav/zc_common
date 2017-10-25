b2b062f27db5b4850b16bfa52f7ad21d11da4fa8
Tony Pilara <tony.pilara@gmail.com>
2017-10-24 17:01:27 -0700

    Read the JSON body from event instead of parsing

--- zc_common/remote_resource/parsers.py
+++ zc_common/remote_resource/parsers.py
@@ -58,7 +58,7 @@ class JSONParser(parsers.JSONParser):
         """
         Parses the incoming bytestream as JSON and returns the resulting data
         """
-        result = super(JSONParser, self).parse(stream, media_type=media_type, parser_context=parser_context)
+        result = stream.raw_body
 
         data = result.get('data')
 
