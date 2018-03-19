64c0f5a2b6cc828d350b2a3123841c58e8bf4aba
carllopez <carl.lopez@gmail.com>
2018-03-18 22:52:29 -0600

    Dropping companies from JWT payload

--- zc_common/jwt_auth/utils.py
+++ zc_common/jwt_auth/utils.py
@@ -16,7 +16,6 @@ def jwt_payload_handler(user):
     payload = {
         'id': encoding.force_text(user.pk),
         'roles': user.get_roles(),
-        'companies': user.get_company_permissions(),
     }
 
     return payload
